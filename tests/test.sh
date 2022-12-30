be3a7f59888366de475b774a7dedb4cd333494c9
Vojtech Vitek <vojtech.vitek@golang.cz>
2022-12-30 19:18:10 +0100

    Run interoperability tests against webrpc-test@v0.10.0 binary

--- tests/test.sh
+++ tests/test.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 PORT=9889
 
 ### Run interoperability tests against specific versions of webrpc.
-for VERSION in v0.9.0 v0.9.1; do
+for VERSION in v0.10.0; do
     WEBRPC_GEN="bin/webrpc-gen@$VERSION.$OS-$ARCH"
     WEBRPC_TEST="bin/webrpc-test@$VERSION.$OS-$ARCH"
 
@@ -16,18 +16,13 @@ for VERSION in v0.9.0 v0.9.1; do
     [[ ! -f $WEBRPC_GEN ]] && curl -o $WEBRPC_GEN -LJO "$WEBRPC_GEN_URL" && chmod +x $WEBRPC_GEN
     [[ ! -f $WEBRPC_TEST ]] && curl -o $WEBRPC_TEST -LJO "$WEBRPC_TEST_URL" && chmod +x $WEBRPC_TEST
 
-    # # Print test RIDL schema from webrpc-test
-    # $WEBRPC_TEST -print-schema > ./test@$VERSION.ridl
-    # # Generate server/client code from RIDL schema
-    # $WEBRPC_GEN -schema=./test@$VERSION.ridl -target=golang -pkg=tests -client -server -out=./test.gen.go
-
     echo "###"
-    echo "### webrpc@$VERSION server <=> generated client"
+    echo "### webrpc@$VERSION reference server <=> generated client"
     echo "###"
     echo
 
-    # Run webrpc-test@VERSION server and wait for it to be ready
-    echo "Running webrpc-test@$VERSION server at 0.0.0.0:$PORT"
+    # Run reference webrpc-test@VERSION server and wait for it to be ready
+    echo "Running reference webrpc-test@$VERSION server at 0.0.0.0:$PORT"
     $WEBRPC_TEST -server -port=$PORT -timeout=2s &
     until nc -z localhost $PORT; do sleep 0.1; done
 
@@ -38,7 +33,7 @@ for VERSION in v0.9.0 v0.9.1; do
     echo
 
     echo "###"
-    echo "### generated server <=> webrpc@$VERSION client"
+    echo "### generated server <=> webrpc@$VERSION reference client"
     echo "###"
     echo
 
@@ -46,8 +41,8 @@ for VERSION in v0.9.0 v0.9.1; do
     go test -v -server=true -client=false -httptest.serve=0.0.0.0:$PORT -serverTimeout=2s &
     until nc -z localhost $PORT; do sleep 0.1; done
 
-    # Run webrpc-test@VERSION client and wait for it to be ready
-    echo "Running webrpc-test@$VERSION client tests against http://localhost:$PORT"
+    # Run reference webrpc-test@VERSION client and wait for it to be ready
+    echo "Running reference webrpc-test@$VERSION client tests against http://localhost:$PORT"
     $WEBRPC_TEST -client -url=http://localhost:$PORT
 
     wait
