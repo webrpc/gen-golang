057657ab7c8112e5a2b507617e94bb0438178223
Vojtech Vitek <vojtech.vitek@golang.cz>
2025-11-09 16:49:01 +0100

    Remove server-only methods from client code

--- _examples/golang-basics/example.gen.go
+++ _examples/golang-basics/example.gen.go
@@ -929,6 +929,29 @@ func RespondWithError(w http.ResponseWriter, err error) {
 	w.Write(respBody)
 }
 
+type method struct {
+	name        string
+	service     string
+	annotations map[string]string
+}
+
+func (m method) Name() string {
+	return m.name
+}
+
+func (m method) Service() string {
+	return m.service
+}
+
+func (m method) Annotations() map[string]string {
+	res := make(map[string]string, len(m.annotations))
+	for k, v := range m.annotations {
+		res[k] = v
+	}
+
+	return res
+}
+
 var methods = map[string]method{
 	"/rpc/Example/Ping": {
 		name:        "Ping",
@@ -970,6 +993,20 @@ var methods = map[string]method{
 		service:     "Example",
 		annotations: map[string]string{},
 	},
+}
+
+func MethodCtx(ctx context.Context) (method, bool) {
+	req := RequestFromContext(ctx)
+	if req == nil {
+		return method{}, false
+	}
+
+	m, ok := methods[req.URL.Path]
+	if !ok {
+		return method{}, false
+	}
+
+	return m, true
 }
 
 func WebrpcMethods() map[string]method {
@@ -1120,29 +1157,6 @@ func HTTPRequestHeaders(ctx context.Context) (http.Header, bool) {
 // Webrpc helpers
 //
 
-type method struct {
-	name        string
-	service     string
-	annotations map[string]string
-}
-
-func (m method) Name() string {
-	return m.name
-}
-
-func (m method) Service() string {
-	return m.service
-}
-
-func (m method) Annotations() map[string]string {
-	res := make(map[string]string, len(m.annotations))
-	for k, v := range m.annotations {
-		res[k] = v
-	}
-
-	return res
-}
-
 type contextKey struct {
 	name string
 }
@@ -1152,11 +1166,14 @@ func (k *contextKey) String() string {
 }
 
 var (
-	HTTPClientRequestHeadersCtxKey = &contextKey{"HTTPClientRequestHeaders"} // client
-	HTTPResponseWriterCtxKey       = &contextKey{"HTTPResponseWriter"}       // server
-	HTTPRequestCtxKey              = &contextKey{"HTTPRequest"}              // server
-	ServiceNameCtxKey              = &contextKey{"ServiceName"}              // server
-	MethodNameCtxKey               = &contextKey{"MethodName"}               // server
+	HTTPClientRequestHeadersCtxKey = &contextKey{"HTTPClientRequestHeaders"}
+)
+
+var (
+	HTTPResponseWriterCtxKey = &contextKey{"HTTPResponseWriter"}
+	MethodNameCtxKey         = &contextKey{"MethodName"}
+	HTTPRequestCtxKey        = &contextKey{"HTTPRequest"}
+	ServiceNameCtxKey        = &contextKey{"ServiceName"}
 )
 
 func ServiceNameFromContext(ctx context.Context) string {
@@ -1172,20 +1189,6 @@ func MethodNameFromContext(ctx context.Context) string {
 func RequestFromContext(ctx context.Context) *http.Request {
 	r, _ := ctx.Value(HTTPRequestCtxKey).(*http.Request)
 	return r
-}
-
-func MethodCtx(ctx context.Context) (method, bool) {
-	req := RequestFromContext(ctx)
-	if req == nil {
-		return method{}, false
-	}
-
-	m, ok := methods[req.URL.Path]
-	if !ok {
-		return method{}, false
-	}
-
-	return m, true
 }
 
 // PtrTo is a useful helper when constructing values for optional fields.
